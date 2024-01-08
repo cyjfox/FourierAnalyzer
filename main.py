a0e950d7dd6d1d2f5a69073ccd54f800323ed1a7
cyjfox <cyjfox@hotmail.com>
2024-01-08 10:44:24 +0800

    add get_fft and generate_test_data functions

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 #import tkinter
 from tkinter import *
+from scipy import fft
+import numpy as np
 
 def run_gui():
     #top = tkinter.Tk()
@@ -22,10 +24,26 @@ def run_gui():
     main_canvas = Canvas(root, width=800, height=600, background="lightblue")
     main_canvas.pack()
 
-    main_canvas.create_line([1, 2, 3, 4, 5, 6, 7, 8, 80, 90, 100, 200])
+    main_canvas.create_line([1, 2, 3, 4, 5, 6, 7, 8, 80, 90, 100, 200, 500, 500])
     root.mainloop()
 
+def get_fft(data, fs):
+    fft_value = fft.fft(data)
+    magnitude = np.abs(fft_value)
+    fft_new = magnitude[:int(len(data) / 2)]
+    fft_new = fft_new / len(data) * 2
+    fft_new[0] = fft_new[0] / 2
+    x = range(int(len(data) / 2))
+    x = [i * fs / len(data) for i in x]
+    return x, fft_new
 
+def generate_test_data(start, end, fs, dc, f1, m1, p1, *args):
+    period = 1 / fs
+    start = (int(start / period) + 1) * period
+    end = (int(end / period) + 1) * period
+    x = range(start, end)
+    y = dc + m1 * np.sin(2 * np.pi * f1 + p1)
+    return y
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
